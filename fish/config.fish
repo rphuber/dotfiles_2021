283df1c1d6532a7b753fe1186f3c8f9683ef2028
Ryan Huber <ryan.huber@benefitfocus.com>
2017-10-04 09:25:08 -0400

    add vi function and cursor changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,3 @@
 status --is-interactive; and source (rbenv init -|psub)
 set fish_greeting ""
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-rbenv rehash >/dev/null ^&1
 nvm use default
