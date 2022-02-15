ad5cdee294d2c7bb189757086571ea14aeb4a5a0
Ryan Huber <ryan.huber@mac.com>
2022-02-15 10:18:23 -0500

    Update fish_right_prompt.fish
    Fixes to right prompt with fish upgrade

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,41 +1,110 @@
 function fish_right_prompt -d "Write out the right prompt"
 
+  set -g glyph_git_has_staged_changes '±' # Alternatives: ~+±
+  set -g glyph_git_has_stashes '≡'
+  set -g glyph_git_has_untracked_files '…' # Alternatives: …☡+±
+  set -g glyph_git_is_ahead '⭱' # Alternatives: ⭱⭡↑⤽⤼
+  set -g glyph_git_is_behind '⭳' # Alternatives: ⭳⭣↓⤽⤼
+  set -g glyph_git_is_dirty '*'
+  set -g glyph_git_is_diverged '🔀' # Alternatives: ⭿⮁⇅ ⤲⤱⤮⤭ 🔄🔀
+  set -g glyph_git_on_branch '🜉' # Alternatives: 🜉⎇
+  set -g glyph_git_on_detached '⌀'
+  set -g glyph_git_on_tag '⌂'
 
-  set -g ICON_NODE             \UE718   " "     # îœ˜ from Devicons or â¬¢
+  set -g ICON_NODE             \UE718   " "     #  from Devicons or ⬢
+  set -g ICON_RUBY                  \UE791" "     # \UE791 from Devicons; \UF047; \UE739; 💎
 
   set -l exit_code $status
-
+  set -l is_git_repository (git rev-parse --is-inside-work-tree 2>/dev/null)
 
   function available -a name -d "Check if a function or program is available."
     #-a NAMES assigns the value of successive command-line arguments to the names given in NAMES
-    type -q $name
+    type "$name" 2>/dev/null >&2
   end
 
-  # node version
-  function _node_version -d "Get the currently used node version if fnm exists"
+  function _node_version -d "Get the currently used node version if NVM exists"
     set -l node_version
     available fnm; and set node_version (string trim -l -c=v (node -v 2>/dev/null)) # trimmed lef 'v'; can use 'nvm current', but slower
     test $node_version; and echo -n -s $ICON_NODE $node_version " "
   end
-  # git prompt
-  set -l git_directory
-  set -l git_dirty
-  set -l git_staged
-  available fish_git_prompt; and set git_directory (fish_git_prompt)
-  available __fish_git_prompt_dirty; and set git_dirty (__fish_git_prompt_dirty)
-  available __fish_git_prompt_staged; and set git_staged (__fish_git_prompt_staged)
+
+  function _ruby_version -d "Get RVM or rbenv version and output" #^&1 stderr2stdout, >&2 vice versa, '>' stdout, '2>' stderr
+    set -l ruby_ver
+        set ruby_ver (rbenv version-name)
+    if test -n (_rbenv_gemset 2>/dev/null; or echo "")
+      test $ruby_ver; and echo -n -s $ICON_RUBY$ruby_ver"@"
+    else
+      test $ruby_ver; and echo -n -s $ICON_RUBY$ruby_ver " "
+    end
+  end
+
+  function _rbenv_gemset -d "Get main current gemset name"
+    if available rbenv
+      if test (rbenv gemset active 2>/dev/null)                           #redirects stderr to /null
+        set -l active_gemset (string split -m1 " " (rbenv gemset active))
+        echo -n -s $active_gemset[1]
+      else
+        echo ''
+      end
+    else
+      echo ''
+    end
+  end
 
   # Print a red dot for failed commands.
   if test $exit_code -ne 0
     set_color red
-    echo -n "✖ "
+    echo -n "• "
     set_color normal
   end
 
   set NODEp (_node_version)
+  set RUBYp (_ruby_version)
   set_color green
   echo -n -s "$NODEp"
+  set_color red
+  echo -n -s "$RUBYp"
 
+  # TODO: Randomly run git fetch in the background?
+  # if test -n $is_git_repository -a (random 0 200) -gt 1
+  #   git fetch &
+  # end
+
+  # Print coloured arrows when git push (up) and / or git pull (down) can be run.
+  #
+  # Red means the local branch and the upstream branch have diverted.
+  # Yellow means there are more than 3 commits to push or pull.
+  if test -n "$is_git_repository"
+    git rev-parse --abbrev-ref '@{upstream}' >/dev/null 2>&1; and set -l has_upstream
+    if set -q has_upstream
+      set -l commit_counts (git rev-list --left-right --count 'HEAD...@{upstream}' 2>/dev/null)
+
+      set -l commits_to_push (echo $commit_counts | cut -f 1 2>/dev/null)
+      set -l commits_to_pull (echo $commit_counts | cut -f 2 2>/dev/null)
+
+      if test $commits_to_push != 0
+        if test $commits_to_pull -ne 0
+          set_color red
+        else if test $commits_to_push -gt 3
+          set_color yellow
+        end
+
+        echo -n "⇡ "
+      end
+
+      if test $commits_to_pull != 0
+        if test $commits_to_push -ne 0
+          set_color red
+        else if test $commits_to_pull -gt 3
+          set_color yellow
+        end
+
+        echo -n "⇣ "
+      end
+
+      set_color normal
+    end
+  end
 
   # Print the username when the user has been changed.
   if test $USER != $LOGNAME
@@ -43,22 +112,35 @@ function fish_right_prompt -d "Write out the right prompt"
     set_color normal
   end
 
-  set_color normal
-
-  if test $git_directory
+  # Print the current git branch name or shortened commit hash in colour.
+  #
+  # Green means the working directory is clean.
+  # Yellow means all changed files have been staged.
+  # Red means there are changed files that are not yet staged.
+  #
+  # Untracked files are ignored.
+  if test -n "$is_git_repository"
+    set_color normal
     echo -n "["
 
-    set_color green --bold
+    set -l branch (git symbolic-ref --short HEAD 2>/dev/null; or git show-ref --head -s --abbrev | head -n1 2>/dev/null)
 
-    if test $git_dirty
+    git diff-files --quiet --ignore-submodules 2>/dev/null; or set -l has_unstaged_files
+    git diff-index --quiet --ignore-submodules --cached HEAD 2>/dev/null; or set -l has_staged_files
+
+    if set -q has_unstaged_files
       set_color red
-    end
-    if test $git_staged
+    else if set -q has_staged_files
       set_color yellow
+    else
+      set_color green
     end
-    echo (fish_git_prompt '%s')
-    set_color normal
-    echo -n "]"
-  end
 
+    echo -n $branch
+
+    set_color normal
+
+    echo  "]"
+
+  end
 end
